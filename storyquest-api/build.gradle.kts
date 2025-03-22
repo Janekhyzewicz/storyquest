4ab682d2d188760f0c797f7871f26840f3ba8448
Janek <63728828+Janekhyzewicz@users.noreply.github.com>
2025-03-22 16:09:48 +0100

    Update build.gradle.kts

--- storyquest-api/build.gradle.kts
+++ storyquest-api/build.gradle.kts
@@ -46,14 +46,4 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
-plugins {
-    id("org.springframework.boot") version "3.2.0"
-    id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.20"
-    kotlin("plugin.spring") version "1.9.20"
-}
 
-// Now bootJar will work
-bootJar {
-    enabled = true
-}
